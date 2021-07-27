03f07bf0d9408b7a72e487e98e9aba8a5ac9df7e
jasminjil <87429569+jasminjil@users.noreply.github.com>
2021-07-27 12:27:20 +0100

    Update fts_keyword_searching.R

--- project_code/fts_keyword_searching.R
+++ project_code/fts_keyword_searching.R
@@ -7,6 +7,7 @@ setwd("C:/Users/jasminj/Documents/R/covid_gender_briefing-main")
 
 fts <- fread("project_code/fts_flows.csv")
 
+names(fts)
 keep <- c(
   "id"
   ,
@@ -21,6 +22,22 @@ keep <- c(
   "sector"
   ,
   "amountUSD"
+  ,
+  "sourceObjects_Organization.name"
+  ,
+  "destinationObjects_Location.name"
+  ,
+  "sourceObjects_Emergency.name"
+  ,
+  "destinationObjects_Emergency.name"
+  ,
+  "sourceObjects_GlobalCluster.name"  
+  ,
+  "destinationObjects_GlobalCluster.name"  
+  ,
+  
+ 
+  
 )
 
 fts <- fts[, ..keep]
@@ -28,11 +45,24 @@ fts <- fts[, ..keep]
 fts <- fts[as.character(year) >= 2014]
 
 major.keywords <- c(
-  "girl.*education",
-  "education.*girl",
-  "gender.*education|education.*gender",
-  "equitable.*education|education.*equitable",
-  "equality.*education|education.*equality"
+   "reproductive.",
+   "contraceptive.",
+  "birth control.",
+  "gender.",
+  "female empowerment.",
+  "domestic violence.",
+  "gender-based.* violence| violence.*gender.",
+  "girl.*women. |women.* girl.",
+  "GBV.",
+  "women.* informal| informal.* women",
+  "women",
+  "woman",
+  "girl.",
+  "female",
+  "maternal",
+  "SRH"
+  
+  
 )
 
 minor.keywords <- c(
@@ -42,14 +72,15 @@ minor.keywords <- c(
 )
 
 disqualifying.keywords <- c(
-  #"keyword5"
-  #,
-  #"keyword6"
+  "\\bmen\\b",
+  "\\bman\\b",
+  "boy.",
+  "male."
+
 )
 
 disqualifying.sectors <- c(
- "promotion of development awareness",
- "material relief assistance and services"
+
 )
 
 fts$relevance <- "None"
@@ -58,12 +89,12 @@ fts[grepl(paste(major.keywords, collapse = "|"), tolower(paste(fts$description))
 
 fts$check <- "No"
 fts[relevance == "Minor"]$check <- "potential false positive"
-fts[relevance != "None"][PurposeName %in% disqualifying.sectors]$check <- "potential false negative"
-fts[relevance != "None"][grepl(paste(disqualifying.keywords, collapse = "|"), tolower(paste(fts[relevance != "None"]$ProjectTitle, fts[relevance != "None"]$ShortDescription, fts[relevance != "None"]$LongDescription)))]$check <- "potential false negative"
+#fts[relevance != "None"][PurposeName %in% disqualifying.sectors]$check <- "potential false negative"
+fts[relevance != "None"][grepl(paste(disqualifying.keywords, collapse = "|"), tolower(paste(fts[relevance != "None"]$ProjectTitle, fts[relevance != "None"]$description, fts[relevance != "None"]$description)))]$check <- "potential false negative"
 
-fts[relevance != "None"][grepl(paste(disqualifying.keywords, collapse = "|"), tolower(paste(fts[relevance != "None"]$ProjectTitle, fts[relevance != "None"]$ShortDescription, fts[relevance != "None"]$LongDescription)))]$relevance <- "Nonefts[relevance != "None"][PurposeName %in% disqualifying.sectors]$relevance <- "None"
+#fts[relevance != "None"][grepl(paste(disqualifying.keywords, collapse = "|"), tolower(paste(fts[relevance != "None"]$ProjectTitle, fts[relevance != "None"]$ShortDescription, fts[relevance != "None"]$LongDescription)))]$relevance <- "Nonefts[relevance != "None"][PurposeName %in% disqualifying.sectors]$relevance <- "None"
 
 fts_output <- fts
 rm(fts)
 
-fts.years <- dcast.data.table(fts_output, year ~ relevance, value.var = "USD_Disbursement_Defl",fun.aggregate = function(x) sum(x, na.rm=T))
+#fts.years <- dcast.data.table(fts_output, year ~ relevance, value.var = "USD_Disbursement_Defl",fun.aggregate = function(x) sum(x, na.rm=T))
