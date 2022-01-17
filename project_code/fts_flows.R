cf9ea21bdf54f99352fa8686d6d9e448cf5153ab
danjwalton <20973540+danjwalton@users.noreply.github.com>
2022-01-17 11:26:03 +0000

    Use new approach for curated FTS flows

--- project_code/fts_flows.R
+++ project_code/fts_flows.R
@@ -2,17 +2,10 @@ suppressPackageStartupMessages(lapply(c("data.table", "jsonlite","rstudioapi"), 
 
 #Load FTS utility functions
 setwd(dirname(getActiveDocumentContext()$path))
-lapply(c("functions/fts_get_flows.R", "functions/fts_unnest_flows.R", "functions/fts_split_rows.R"), source)
-
 setwd("..")
 
-#Download FTS flows and unnest from JSON
-fts_raw <- fts_get_flows(year = c(2019,2020,2021))
-fts <- fts_unnest_flows(fts_raw)
-
-#Split rows into individual years where multiple are recorded
-fts[, year := sourceObjects_UsageYear.name]
-fts <- fts_split_rows(fts, value.cols = "amountUSD", split.col = "year", split.pattern = "; ", remove.unsplit = T)
+source("https://raw.githubusercontent.com/devinit/gha_automation/main/IHA/fts_curated_flows.R")
+fts <- fts_curated_flows(years = 2018:2021, update = NA, dataset_path = "project_data")
 
 #Assign a sector from available fields
 fts[, sector := destinationObjects_GlobalCluster.name]
@@ -20,9 +13,9 @@ fts[is.na(sector) & !is.na(sourceObjects_GlobalCluster.name), sector := sourceOb
 fts[is.na(sector) & !is.na(destinationObjects_Cluster.name), sector := destinationObjects_Cluster.name]
 
 #Split rows into individual sectors where multiple are recorded
-fts <- fts_split_rows(fts, value.cols = "amountUSD", split.col = "sector", split.pattern = "; ", remove.unsplit = T)
+fts <- fts_split_rows(fts, value.cols = "amountUSD_defl", split.col = "sector", split.pattern = "; ", remove.unsplit = T)
 
-fts <- fts[year %in% c(2019, 2020, 2021)]
+fts <- fts[year %in% c(2018, 2019, 2020, 2021)]
 fts[is.null(fts) | fts == "NULL"] <- NA
 fts[, `:=` (reportDetails = NULL, childFlowIds = NULL)]
 
--- project_code/fts_keyword_searching.R
+++ project_code/fts_keyword_searching.R
@@ -7,9 +7,6 @@ setwd(dirname(getActiveDocumentContext()$path))
 setwd("..")
 
 fts <- fread("project_data/fts_flows.csv", encoding = "UTF-8")
-rec_codes <- fread("project_data/fts_recipientcodename.csv", encoding = "UTF-8")
-channels <- fread("project_data/fts_deliverychannels.csv", encoding = "UTF-8")
-ngo_types <- fread("project_data/fts_ngotype.csv", encoding = "UTF-8")[ngotype == "Southern International NGO", ngotype := "Southern international NGO"][ngotype == "Undefined NGO", ngotype := "Uncategorized NGO"]
 source("https://raw.githubusercontent.com/devinit/di_script_repo/main/gha/FTS/fts_split_rows.R")
 
 keep <- c(
@@ -284,53 +281,7 @@ sector.decode[is.na(new_sector), new_sector := "Other"]
 
 fts_output[sector %in% sector.decode$sector, sector := merge(fts_output[sector %in% sector.decode$sector, .(sector)], sector.decode, by = "sector")$new_sector]
 fts_output[sector == "", sector := "Unspecified"]
-#fts_output[grepl("Protection", sector), sector := "Protection"]
+fts_output[grepl("Protection", sector), sector := "Protection"]
 
-#Merge recipient code lists
-rec_orgs <- merge(ngo_types, channels, by = c("Recipient.Organization", "year"), all = T)
-rec_orgs <- merge(rec_orgs, rec_codes, by = c("Recipient.Organization", "year"), all = T)
-rec_orgs[is.na(rec_orgs)] <- ""
-
-rec_orgs <- unique(rec_orgs[, -"year"])
-
-fts_output <- merge(fts_output[, destinationObjects_Organization.name := gsub(", Government of", "", destinationObjects_Organization.name)], rec_orgs, by.x = c("destinationObjects_Organization.name"), by.y = c("Recipient.Organization"), all.x = T)
-
-#NGO classification for those missing in codelist
-fts_output[ngotype == "" & destinationObjects_Organization.organizationTypes == "NGO", ngotype := gsub("^ |NGO |", "", paste(destinationObjects_Organization.organizationSubTypes, destinationObjects_Organization.organizationTypes))]
-fts_output[ngotype == "NGO", ngotype := "Uncategorized NGO"]
-fts_output[grepl(";", ngotype), ngotype := "Other"]
-fts_output[is.na(ngotype), ngotype := ""]
-
-#Split rows into individual donors where multiple are recorded
-fts_output <- fts_split_rows(fts_output, value.cols = "amountUSD", split.col = "sourceObjects_Organization.id", split.pattern = "; ", remove.unsplit = T)
-
-fts_orgs <- data.table(fromJSON("https://api.hpc.tools/v1/public/organization")$data)
-fts_orgs[, `:=` (type = ifelse(is.null(categories[[1]]$name), NA, categories[[1]]$name), location = ifelse(is.null(locations[[1]]$name), NA, locations[[1]]$name)), by = id]
-
-fts_orgs_gov <- fts_orgs[type == "Government", .(sourceObjects_Organization.id = id, donor_country = location)]
-
-#Manual development agency locations
-fts_orgs_gov <- rbind(fts_orgs_gov,
-                      data.table(sourceObjects_Organization.id = c("9946", "10399", "4058", "2987", "30", "6547"),
-                                 donor_country = c("France", "Qatar", "United States", "Germany", "United Arab Emirates", "Taiwan, Province of China"))
-)
-
-#Merge orgs types for deflators
-fts_output <- merge(fts_output, fts_orgs_gov, by = "sourceObjects_Organization.id", all.x = T)
-fts_output[is.na(donor_country), donor_country := "Total DAC"]
-
-#Add year where none recorded at source
-fts_output[is.na(year), year := budgetYear]
-
-deflators <- fread("project_data/usd_deflators_2021WEO.csv", encoding = "UTF-8", header = T)
-deflators <- melt.data.table(deflators, id.vars = c("name", "ISO3"))
-deflators <- deflators[, .(donor_country = name, year = as.numeric(as.character(variable)), deflator = value)]
-
-fts_output <- merge(fts_output, deflators, by = c("donor_country", "year"), all.x = T)
-
-fts_output[is.na(deflator)]$deflator <- merge(fts_output[is.na(deflator)][, -"deflator"], deflators[donor_country == "Total DAC", -"donor_country"], by = "year")$deflator
-
-fts_output[, amountUSD_defl := amountUSD/deflator]
-
-write.csv(fts_output, "fts_output.csv", fileEncoding = "UTF-8")
+write.csv(fts_output, "fts_output.csv", fileEncoding = "UTF-8", row.names = F)
 #
